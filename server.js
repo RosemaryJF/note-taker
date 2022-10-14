f73aec22f540214a80514e5568d8ed85691144be
RJ Dust Puppies <109449181+RosemaryJF@users.noreply.github.com>
2022-10-14 14:32:19 +1100

    Furhter file additions and beginning of server.js work

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
 const express = require('express');
-const 
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Navigate to /notes'));
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.listen(PORT, () =>
+    console.log(`App is running at http://localhost:${PORT}`)
+);

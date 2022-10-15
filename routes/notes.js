58a1c1028520f2c0aca65c2420540fab82d9a430
RJ Dust Puppies <109449181+RosemaryJF@users.noreply.github.com>
2022-10-15 16:23:10 +1100

    Rework of delete function

--- routes/notes.js
+++ routes/notes.js
@@ -4,7 +4,7 @@ const uuid = require('../helpers/uuid');
 
 // GET Route for retrieving all notes
 notes.get('/', (req, res) => {
-  console.info(`${req.method} request received for new note`);
+  console.info(`${req.method} request received to retrieve notes`);
 
   readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
 });
@@ -42,13 +42,27 @@ notes.post('/', (req, res) => {
 notes.delete('/', (req, res) => {
   // Log that a POST request was received
   console.info(`${req.method} request received to delete note`);
+    
+  let jsonFilePath = path.join(__dirname, "./db/notes.json");
+    // request to delete note by id.
+    for (let i = 0; i < notes.length; i++) {
 
-  let db = JSON.parse(fs.readFileSync('db/notes.json'))
-  // removing note with id
-  let deleteNote = db.filter(item => item.id !== req.params.id);
-  // Rewriting note to db.json
-  fs.writeFileSync('db/notes.json', JSON.stringify(deleteNote));
-  res.json(deleteNote);
+        if (notes[i].id == req.params.id) {
+            // Splice takes i position, and then deletes the 1 note.
+            notes.splice(i, 1);
+            break;
+        }
+    }
+    // Write the db.json file again.
+    fs.writeFileSync(jsonFilePath, JSON.stringify(notes), function (err) {
+
+        if (err) {
+            return console.error(err);
+        } else {
+            console.log("The selected note was deleted!");
+        }
+    });
+    res.json(notes);
 });
 
 module.exports = notes;
